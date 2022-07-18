c8ece0ed383fe0331ae7cac6d845820aca3f2414
Sam Vermet <sam.vermet@ikologik.com>
2022-07-18 14:28:13 +0200

    Language buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '../en-BE'},
+  {path: '../en-US'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
